2854716145eda23425fc0a059c202fa37b9dfc99
Chris Reading <chrisreading.email@gmail.com>
2017-10-26 17:18:12 -0600

    Added function to parse created_at time and changed convertMillis function name

--- app/util.js
+++ app/util.js
@@ -3,13 +3,19 @@
 module.exports = {
     /**
      * Converts millseconds to minutes and seconds
-     * returns a string, ex: "4:44"
-     * 
-     * kinda long function name, I know.
+     * ex: "4:44"
      */
-    convertMillisToMinutesAndSeconds: function(millis) {
+    convertMillisToTime: function(millis) {
         var minutes = Math.floor(millis / 60000);
         var seconds = ((millis % 60000) / 1000).toFixed(0)
         return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+    },
+
+    /**
+     * Cut the time from created_at in stream response
+     * ex: 2017-10-26T13:17:22Z -> 13:17:22
+     */
+    getStartTime: function(created_at) {
+        return (created_at.split('T').pop()).slice(0, -1)
     }
 }
