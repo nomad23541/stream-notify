130e1214bef9efb98989dfdf8ff83061580db7bf
Chris Reading <chrisreading.email@gmail.com>
2017-10-26 17:38:53 -0600

    Changed convertMillisToTime to include hours

--- app/util.js
+++ app/util.js
@@ -3,12 +3,29 @@
 module.exports = {
     /**
      * Converts millseconds to minutes and seconds
-     * ex: "4:44"
+     * ex: "00:04:44"
      */
     convertMillisToTime: function(millis) {
-        var minutes = Math.floor(millis / 60000);
-        var seconds = ((millis % 60000) / 1000).toFixed(0)
-        return minutes + ':' + (seconds < 10 ? '0' : '') + seconds
+        var hours = Math.floor(millis / 3600000)
+        var minutes = Math.floor((millis - (hours * 3600000)) / 60000)
+        var seconds = parseInt((millis - (hours * 3600000) - (minutes * 60000)) / 1000)
+
+        if(hours < 10)
+            hours = '0' + hours
+        if(minutes < 10)
+            minutes = '0' + minutes      
+        if(seconds < 10)
+            seconds = '0' + seconds
+
+        return hours + ':' + minutes + ':' + seconds
+    },
+
+    /**
+     * Converts time to millseconds
+     * ex: "4:44" -> 4399528392 (not really but yeah)
+     */
+    convertTimeToMillis: function(time) {
+
     },
 
     /**
