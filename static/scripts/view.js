4985b552aefe209cc8c9965d62729f8b79f13e75
Chris Reading <chrisreading.email@gmail.com>
2017-10-27 22:50:22 -0600

    Completely refactored how notifications look and appear

--- static/scripts/view.js
+++ static/scripts/view.js
@@ -1,41 +1,57 @@
 $(document).ready(function() {
     var socket = io();
 
-    socket.on('subscribe', function(msg) {
-        window.location.href = 'subscribe';
+    socket.on('subscribe', function(data) {
+        subscribe(data.username);
     });
 
-    socket.on('resub', function(msg) {
-        window.location.href = 'resub';
+    socket.on('resub', function(data) {
+        resub(data.username, data.months);
     });
     
-    socket.on('hosted', function(msg) {
-        window.location.href = 'hosted';
+    socket.on('hosted', function(data) {
+        hosted(data.username, data.viewers);
     });
 
     socket.on('timer', function(data) {
-        console.log('timer has started with data: ' + data.msg);
+        timer(data.time);
     });
-});
 
-function subscribe() {
-    var audio = document.createElement('audio');
-    audio.setAttribute('src', 'sound/subscribe-notification.mp3');
+    function subscribe(username) {
+        var audio = document.createElement('audio');
+        audio.setAttribute('src', 'sound/subscribe-notification.mp3');
     
-    var container = $('#subscribe-notify');
-    container.fadeIn(0);
-    container.addClass('animated bounceInLeft');
-    audio.play();
-    container.delay(8000).fadeOut('slow', function() { container.removeClass('animated bounceInLeft'); });
-}
+        var container = $('#subscribe');
+        container.find('.notif-main').text(username);
+        container.fadeIn(0);
+        container.addClass('animated bounceInLeft');
+        audio.play();
+        container.delay(8000).fadeOut('slow', function() { container.removeClass('animated bounceInLeft'); });
+    }
 
-function hosted() {
-    var container = $('#host-notify');
-    container.fadeIn(0);
-    container.addClass('animated bounceInRight');
-    container.delay(8000).fadeOut('slow', function() { container.removeClass('animated bounceInRight'); });
-}
+    function resub(username, months) {
+        var container = $('#resub');
+        container.find('.notif-main').text(username);
+        container.find('.notif-footer').text('resubbed for ' + months + ' months!');
+        container.fadeIn(0);
+        container.addClass('animated bounceInLeft');
+        container.delay(8000).fadeOut('slow', function() { container.removeClass('animated bounceInLeft'); });
+    }
 
-function counter() {
-    var container = $('#counter-notify');
-}
+    function hosted(username, viewers) {
+        var container = $('#hosted');
+        container.find('.notif-plain b#username').text(username);
+        container.find('.notif-plain b#viewers').text(viewers);
+        container.fadeIn(0);
+        container.addClass('animated bounceInRight');
+        container.delay(8000).fadeOut('slow', function() { container.removeClass('animated bounceInLeft'); });
+    }
+
+    function timer(time) {
+        var container = $('#timer');
+        container.find('.notif-plain b#time').text(time);
+        container.fadeIn(0);
+        container.addClass('animated bounceInRight');
+        container.delay(8000).fadeOut('slow', function() { container.removeClass('animated bounceInLeft'); });
+    }
+});
