2e783e2ac777a392fc8c91e638f0edbfcc236a34
Chris Reading <chrisreading.email@gmail.com>
2017-10-27 22:51:16 -0600

    Updated the socket.io emits

--- twitch.js
+++ twitch.js
@@ -28,28 +28,20 @@ module.exports = function(app, io) {
     /** Listen for certain events */
     // on subscription
     client.on('subscription', function(channel, username, method, message, userstate) {
-        app.set('username', username)
+        io.emit('subscribe', { username: username })
 
         // unsure if this is a correct call
         //app.set('plan', method.plan)
-
-        io.emit('subscribe', { for: 'everyone' })
     }) 
 
     // on resub
     client.on('resub', function(channel, username, months, message, userstate, methods) {
-        app.set('username', username)
-        app.set('months', months)
-
-        io.emit('resub', { for: 'everyone' })
+        io.emit('resub', { username: username, months: months })
     })
 
     // on hosted
     client.on('hosted', function(channel, username, viewers, autohost) {
-        app.set('username', username)
-        app.set('viewers', viewers)
-
-        io.emit('hosted', { for: 'everyone' })
+        io.emit('hosted', { username: username, viewers: viwers })
     })
 
 
@@ -125,7 +117,7 @@ module.exports = function(app, io) {
             // tell view to show the timer, but only once each hour
             if(minutesUntilNextHour == 5 && !alreadySent) {
                 console.log('5 minutes until ' + nextHour)
-                io.emit('timer', { msg: util.convertMillisToTime(totalTime) })
+                io.emit('timer', { time: util.convertMillisToTime(totalTime) })
                 alreadySent = true
             } 
 
